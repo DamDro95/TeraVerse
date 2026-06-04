67f0d5fb9b71d4752a1dbcff0403d9789050fc88
Damian <damian.drozd@hotmail.com>
2026-06-04 13:58:18 -0400

    Updated collisions

--- player/camera/camera_follow.gd
+++
@@ -1,8 +0,0 @@
-extends Camera3D
-
-@onready var spring_arm := $"../SpringArm3D/SpringArmPosition"
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#saif not multiplayer.is_server(): return
-	position = lerp(position, spring_arm.position, delta*5.0)
