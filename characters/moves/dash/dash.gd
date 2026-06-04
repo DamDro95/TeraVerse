26c42d98aed30c3656b24ddcf5c16800269cc21c
Damian <damian.drozd@hotmail.com>
2026-06-04 17:03:03 -0400

    Added player state machine

--- characters/moves/dash/dash.gd
+++ characters/moves/dash/dash.gd
@@ -1,10 +1,9 @@
 extends Node3D
 
-var body: CharacterBody3D
-
 # Use an Enum to cleanly define your input states
 enum DashDirection { NONE, LEFT, RIGHT, FORWARD, BACK }
 
+@export var player: PlayerController
 @export var double_tap_timeout: float = 0.50
 
 @export_group("Dash Physics")
@@ -34,68 +33,48 @@ const VECTOR_MAP = {
 var last_press_time = 0.0
 var last_keycode = 0
 
-func _ready() -> void:
-	body = get_parent() as CharacterBody3D
-	if not body:
-		push_error("DoubleJumpComponent must be a child of a CharacterBody3D!")
-
 func _input(event: InputEvent) -> void:
-	if not is_multiplayer_authority(): return
+	if not player or not player.is_multiplayer_authority(): return
 	
-	# Check if the event matches ANY action in our lookup keys
 	for action in DIRECTION_MAP.keys():
 		if event.is_action_pressed(action):
 			var pressed_dir = DIRECTION_MAP[action]
-			if event.is_action_pressed("dash") or (last_press_time > 0 and last_keycode == event.keycode):
-				start_dash(VECTOR_MAP[pressed_dir])
+			if last_press_time > 0 and last_keycode == event.keycode:
+				dash(VECTOR_MAP[pressed_dir])
 			else:
 				last_press_time = double_tap_timeout
 				last_keycode = event.keycode
 			break
-			
-func _process(delta: float) -> void:
-	last_press_time -= delta
 
-# --- The start_dash function called by your input handler ---
-func start_dash(relative_vector: Vector3) -> void:
-	is_dashing = true
-	dash_timer = dash_duration
-	
-	# 1. Grab the camera's orientation matrix (basis)
-	var cam_basis = body.camera.global_transform.basis
-	
-	# 2. Multiply the camera view by our intent vector (e.g., Vector3.LEFT)
-	var world_direction = cam_basis * relative_vector
-	
-	# 3. CRITICAL: Flatten the Y axis. If the camera is tilted down, 
-	# this stops the forward dash from driving the player down into the floor.
-	world_direction.y = 0
-	
-	# 4. Normalize it so diagonal calculations don't accidentally make you move faster
-	dash_direction = world_direction.normalized()
-
-# --- Hook this into your physics loop to execute the high-speed movement ---
 func _physics_process(delta: float) -> void:
+	if not player or not player.is_multiplayer_authority(): return
 	
-	if is_dashing:
-		process_dash(delta)
-		return # BYPASS standard WASD walking and gravity during a dash!
+	if player.current_state == player.MoveState.DASHING:
+		return
+		
+	last_press_time -= delta
+	
+	if Input.is_action_just_pressed("dash"):
+		var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+		var direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		dash(direction)
 
-	# ... Your normal walking movement and gravity logic live down here ...
-	# var input_dir = Input.get_vector(...)
-	# move_and_slide()
 
-func process_dash(delta: float) -> void:
-	dash_timer -= delta
+func dash(direction: Vector3) -> void:
+	player.change_state(player.MoveState.DASHING)
 	
-	# Inject the camera-relative dash vector directly into horizontal velocity
-	body.velocity.x = dash_direction.x * dash_speed
-	body.velocity.z = dash_direction.z * dash_speed
-	#body.velocity.y = 0 # Lock gravity to 0 for a crisp, airborne air-dash effect
+	if direction == Vector3.ZERO:
+		direction = -player.transform.basis.z # Forward
 	
-	# Clean exit when the dash duration ends
-	if dash_timer <= 0:
-		is_dashing = false
-		# Kill horizontal momentum instantly so you don't wildly slide forever
-		body.velocity.x = 0
-		body.velocity.z = 0
+	print(direction)
+	var cam_basis = player.camera.global_transform.basis
+	var world_direction = cam_basis * direction
+	world_direction.y = 0
+	direction = world_direction.normalized()
+	
+	player.velocity = direction * dash_speed
+	
+	await player.get_tree().create_timer(dash_duration).timeout
+	
+	if player.current_state == player.MoveState.DASHING:
+		player.change_state(player.MoveState.IDLE_WALK)
--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+class_name PlayerController
 
 @export var health: Node3D
 
@@ -6,14 +7,17 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const DAMAGE_BUFFER = 1.5
 
+enum MoveState { IDLE_WALK, JUMPING, DOUBLE_JUMPING, FLYING, DASHING }
+var current_state: MoveState = MoveState.IDLE_WALK
+
 @onready var camera := $SpringArmPivot/Camera3D
 @onready var animation_player = $AnimationPlayer
 @onready var mesh := $Barbarian
 
-var damage_buffer_time = 0
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
+
 
 func _ready() -> void:
 	if health:
@@ -27,18 +31,16 @@ func _ready() -> void:
 		camera.clear_current() 
 		camera.queue_free() # Or just delete it to keep the scene clean
 
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("attack") and is_on_floor():
 		animation_player.play("Player/Melee_1H_Attack_Chop")
-		
+
+
 func _physics_process(delta: float) -> void:
 	
 	if not is_multiplayer_authority(): return
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -52,13 +54,25 @@ func _physics_process(delta: float) -> void:
 	if not target_angle == 0.0:
 		mesh.rotation.y = lerp_angle(mesh.rotation.y, target_angle, 0.2)
 	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
+	match current_state:
+		MoveState.IDLE_WALK, MoveState.JUMPING, MoveState.DOUBLE_JUMPING:
+			# Apply normal gravity when on the ground or jumping
+			if not is_on_floor():
+				velocity += get_gravity() * delta
+			if direction:
+				velocity.x = direction.x * SPEED
+				velocity.z = direction.z * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+				velocity.z = move_toward(velocity.z, 0, SPEED)
+		MoveState.FLYING:
+				# Zero gravity handled here or inside the Fly Component
+				pass
+			
+		MoveState.DASHING:
+			# Freeze gravity entirely during a dash
+			velocity.y = 0
+			
 	# --- ANIMATION LOGIC ---
 	# Check if we are on the floor and moving horizontally
 	if is_on_floor():
@@ -67,17 +81,17 @@ func _physics_process(delta: float) -> void:
 		elif velocity.length() == 0 and animation_player.current_animation != "Player/Melee_1H_Attack_Chop":
 			animation_player.play("Player/Idle_B")
 			
-	#for i in get_slide_collision_count():
-		#var collision = get_slide_collision(i)
-		#var collider = collision.get_collider()
-#
-		#if collider and collider.has_method("is_in_group"):
-			#if collider.is_in_group("enemy") and health and damage_buffer_time <= 0:
-				#damage_buffer_time = DAMAGE_BUFFER
-				#health.take_damage(collider.damage)
-	
-	damage_buffer_time -= delta
 	move_and_slide()
-	
+
+
 func die() -> void:
 	queue_free()
+	
+## Helper function for components to safely request a state change
+func change_state(new_state: MoveState) -> void:
+	# Prevent interrupting a dash unless you want them to cancel it
+	if current_state == MoveState.DASHING and new_state != MoveState.IDLE_WALK:
+		return
+		
+	current_state = new_state
+	print("Player State Changed to: ", MoveState.keys()[new_state])
